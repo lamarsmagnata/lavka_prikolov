058aef0dab9c29a104b85d36ba1e5e76b6ae2842
Marie Ardenn <aragornJH@live.com>
2024-03-07 08:15:04 +0300

    feat(product): added product model

---
+++ lavka_prikolov/prilavok/models/__init__.py
@@ -0,0 +1,2 @@
+from product import *  # NOQA
+from seller import *  # NOQA
---
+++ lavka_prikolov/prilavok/models/product.py
@@ -0,0 +1,17 @@
+from django.db import models
+from seller import Seller
+
+
+class Poduct(models.Model):
+    name = models.CharField(max_lenght=120)
+    description = models.TextField()
+    price = models.IntegerField()
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-name']
+
+    def __str__(self):
+        return self.name
