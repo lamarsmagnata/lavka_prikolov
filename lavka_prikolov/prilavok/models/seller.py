e21fb3c91cbdbaa82ac7935765f0db99bd9b600c
Marie Ardenn <aragornJH@live.com>
2024-03-07 08:15:55 +0300

    feat(seller): created seller models

---
+++ lavka_prikolov/prilavok/models/seller.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Seller(models.Model):
+    name = models.CharField(max_lenght=120)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-title']
+
+    def __str__(self):
+        return self.name
