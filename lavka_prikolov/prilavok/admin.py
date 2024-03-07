26dbacab9ea695f0eff18899e5f1162380f00eb5
Marie Ardenn <aragornJH@live.com>
2024-03-07 08:59:42 +0300

    feat(admin): registered Customer, Seller, Product

--- lavka_prikolov/prilavok/admin.py
+++ lavka_prikolov/prilavok/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer, Seller, Product
+
+admin.site.register(Customer)
+admin.site.register(Seller)
+admin.site.register(Product)
