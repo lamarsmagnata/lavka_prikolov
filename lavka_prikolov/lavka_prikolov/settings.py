38372e9913c71f93b6d85836e99339dc4cdeef58
Marie Ardenn <aragornJH@live.com>
2024-03-11 08:11:02 +0300

    feat(settings): addued custom_user app to settimgs, added flag

--- lavka_prikolov/lavka_prikolov/settings.py
+++ lavka_prikolov/lavka_prikolov/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'prilavok.apps.PrilavokConfig',
+    'custom_user.apps.CustomUserConfig'
 ]
+
+AUTH_USER_MODEL = 'custom_user.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
